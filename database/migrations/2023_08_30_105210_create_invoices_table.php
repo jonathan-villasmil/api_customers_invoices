f370c051cae41e54d411730b167d9fc43d18497e
jonathan <jonathanvillasmildeabreu@gmail.com>
2023-08-30 13:27:49 +0200

    Config tables

--- database/migrations/2023_08_30_105210_create_invoices_table.php
+++ database/migrations/2023_08_30_105210_create_invoices_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
+            $table->integer('customer_id');
+            $table->integer('amount');
+            $table->string('status'); //billed,  paid, void
+            $table->dateTime('billed_dated');
+            $table->dateTime('paid_dated')->nullable();
             $table->timestamps();
         });
     }
